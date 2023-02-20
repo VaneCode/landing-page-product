48a30cb667b07d3f5ddcc5a036d190999c6ecd79
vanessaBonito <vanebonito@hotmail.com>
2023-02-20 18:37:21 -0500

    Add props to update color in shoe 3D

--- src/App.js
+++ src/App.js
@@ -32,27 +32,36 @@ function App() {
               <Canvas>
                 <Suspense fallback={null}>
                   <ambientLight/>
-                  <Model/>
+                  <spotLight intensity={0.9} angle={0.1} penumbra={1} 
+                            position={[10,15,10]}
+                            castShadow />
+                  <Model customColors={{mesh:mesh, stripes:stripes, soul:soul}}/>
+                  <OrbitControls enablePan={true}
+                                 enableZoom={true}
+                                 enableRotate={true}/>
                 </Suspense>
               </Canvas>
             </div>
             <h2>Color chooser</h2>
             <div className='colors'>
                  <div>
-                    <input type="color" id="head" name="head"
-                           value="#e66465" />
-                    <label for="head">Main</label>
+                    <input type="color" id="mesh" name="mesh"
+                           value={mesh} 
+                           onChange={(e) => setMesh(e.target.value)}/>
+                    <label for="mesh">Main</label>
                   </div>
 
                 <div>
-                    <input type="color" id="body" name="body"
-                            value="#f6b73c" />
-                    <label for="body">Stripes</label>
+                <input type="color" id="stripes" name="stripes"
+                           value={stripes} 
+                           onChange={(e) => setStripes(e.target.value)}/>
+                    <label for="stripes">Stripes</label>
                 </div>
                  <div>
-                    <input type="color" id="body" name="body"
-                            value="#f6b73c" />
-                    <label for="body">Soul</label>
+                 <input type="color" id="soul" name="soul"
+                           value={soul} 
+                           onChange={(e) => setSoul(e.target.value)}/>
+                    <label for="soul">Soul</label>
                 </div>
             </div>
         </div>
