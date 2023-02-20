a0b656edb00819a601c2000faf6e6278871c097c
vanessaBonito <vanebonito@hotmail.com>
2023-02-20 16:57:40 -0500

    Added color chooser component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,30 @@ function App() {
         <boxGeometry attach='geometry' args={[2,2,2]}/>
       </mesh>
      </Canvas>
+     <div class="wrapper">
+        <div class="card">
+            <div class="product-canvas"></div>
+            <h2>Color chooser</h2>
+            <div class='colors'>
+                 <div>
+                    <input type="color" id="head" name="head"
+                           value="#e66465" />
+                    <label for="head">Main</label>
+                  </div>
+
+                <div>
+                    <input type="color" id="body" name="body"
+                            value="#f6b73c" />
+                    <label for="body">Stripes</label>
+                </div>
+                 <div>
+                    <input type="color" id="body" name="body"
+                            value="#f6b73c" />
+                    <label for="body">Soul</label>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
