d3461217970aea0250a0bbce0b91637e64f8ec87
vanessaBonito <vanebonito@hotmail.com>
2023-02-20 18:00:12 -0500

    Add Model function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import './index';
 import { useRef, useState } from 'react';
 import {Canvas} from '@react-three/fiber';
 import {OrbitControls, useGLTF} from '@react-three/drei';
-import { BoxGeometry } from 'three';
+
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  return (
+    <group ref={group} {...props} dispose={null} scale={3}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={props.customColors.setStripes}/>
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={props.customColors.mesh}/>
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={props.customColors.soul}/>
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner}material-color={props.customColors.soul} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={props.customColors.soul}/>
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes}material-color={props.customColors.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={props.customColors.stripes}/>
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={props.customColors.soul}/>
+    </group>
+  )
+}
 
 function App() {
   return (
@@ -11,7 +27,7 @@ function App() {
         <div className="card">
             <div class="product-canvas">
               <Canvas>
-                
+
 
               </Canvas>
             </div>
